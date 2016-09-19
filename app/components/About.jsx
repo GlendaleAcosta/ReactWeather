2994d9cadcbf8a85692e0c8b8afaae1c5b4e68f9
Glendeezy <gglendale17@gmail.com>
2016-09-19 03:40:53 -0700

    Switch to SCSS

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -11,7 +11,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>Welcome to my React Weather Application. This app is from the complete react web app develeoper udemy course and it's been great! ;D</p>
       <p>Here are some of the tools I used:</p>
       <ul>
